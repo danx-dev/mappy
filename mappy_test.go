fc1f22c982ac4efe6af292cac0265509b3741841
Dan Møller <dfm@yoyoglobal.com>
2021-07-16 08:25:49 +0200

    Update mappy_test.go

--- mappy_test.go
+++ mappy_test.go
@@ -9,10 +9,16 @@ import (
 
 type S1 struct {
 	Value1 string `mappy:"Value1"`
+	Value2 string `mappy:"Sub.Value"`
 }
 
 type S2 struct {
 	Value1 string
+	Sub    Sub
+}
+
+type Sub struct {
+	Value string
 }
 
 func TestSimpleMapping(t *testing.T) {
@@ -24,5 +30,17 @@ func TestSimpleMapping(t *testing.T) {
 	fmt.Println("Output", output)
 	if output.Value1 != "Test" {
 		t.Error("Value1 was not mapped!")
+	}
+}
+
+func TestDeepMapping(t *testing.T) {
+	input := S1{Value1: "Test", Value2: "GoSubMapping"}
+	output := S2{}
+
+	mappy.DoMap(&output, &input)
+
+	fmt.Println("Output", output)
+	if output.Sub.Value != "GoSubMapping" {
+		t.Error("Failed to Map Nested Struct")
 	}
 }
