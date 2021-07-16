771c3415e4556dbea4ffd2fd34dd868a9664e367
Dan Møller <dfm@yoyoglobal.com>
2021-07-16 08:19:09 +0200

    Update mappy_test.go

--- mappy_test.go
+++ mappy_test.go
@@ -20,11 +20,14 @@ func TestSimpleMapping(t *testing.T) {
 	input := S1{Value1: "Test"}
 	output := S2{Value1: ""}
 
-	result := mappy.DoMap(&input, &output)
+	result := mappy.DoMap(&output, &input)
 	s, _ := json.Marshal(result)
 	fmt.Println("Json", string(s))
 
 	fmt.Println("Output", output)
+	if output.Value1 != "Test" {
+		t.Error("Value1 was not mapped!")
+	}
 
 	fmt.Println("Result", result)
 }
